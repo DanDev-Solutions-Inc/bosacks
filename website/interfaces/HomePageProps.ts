dfe0f5c880a9d8a8d9b9abbbe4f73566137a7cb7
dank <dank@nightshift.net>
2023-03-02 19:49:19 -0500

    wire up image
    wire up image

---
+++ website/interfaces/HomePageProps.ts
@@ -0,0 +1,7 @@
+import { Configuration } from "./sanity/Configuration";
+import { HomePage } from "./sanity/HomePage";
+
+export interface HomePageProps {
+  page: HomePage;
+  configuration: Configuration;
+}
--- website/pages/index.tsx
+++ website/pages/index.tsx
@@ -1,12 +1,36 @@
-import Head from "next/head";
+import { GetServerSideProps, NextPage } from "next";
+import { useMemo } from "react";
+import { NextSeo } from "next-seo";
 
-export default function Home() {
+import { client } from "@/client";
+import { HomePageProps } from "@/interfaces/HomePageProps";
+import { Configuration } from "@/interfaces/sanity/Configuration";
+import { HomePage } from "@/interfaces/sanity/HomePage";
+
+const Home: NextPage<HomePageProps> = ({
+  page,
+  configuration,
+}: HomePageProps) => {
+  console.log(page, configuration);
+  useMemo(() => {}, []);
+
   return (
     <>
-      <Head>
-        <title>Bosacks</title>
-      </Head>
+      <NextSeo title={page.title} description="" />
       <h1 className="text-3xl font-bold underline">Bosacks.com</h1>
     </>
   );
-}
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const page: HomePage = await client.fetch(`*[_type == "homePage"][0]`);
+  const configuration: Configuration = await client.fetch(
+    `*[_type == "configuration"][0]`
+  );
+
+  return {
+    props: { page, configuration },
+  };
+};
+
+export default Home;
