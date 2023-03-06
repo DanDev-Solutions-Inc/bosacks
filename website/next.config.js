e5474482d496276b4089fd9fb506c8df07e4d78d
dank <dank@nightshift.net>
2023-03-06 13:45:59 -0500

    Update next.config.js

--- website/next.config.js
+++ website/next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["cdn.sanity.io"],
+    domains: ["cdn.sanity.io", "bosacks.vercel.app"],
   },
   env: {
     SANITY_PROJECT_ID: process.env.SANITY_PROJECT_ID,
